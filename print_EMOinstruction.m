5475d27d60c942df6acff88f04a57a101c545db3
fuel_lee <lifuyu198919@gmail.com>
2013-09-05 16:19:47 +0800

    init project

---
+++ print_EMOinstruction.m
@@ -0,0 +1,41 @@
+function print_EMOinstruction()
+% EMOinstruction: display the instruction for the test problems
+% Authors: Maoguo Gong and Licheng Jiao
+% April 7, 2006
+% Copyright (C) 2005-2006 by Maoguo Gong (e-mail: gong@ieee.org)
+%--------------------------------------------------------------------------
+
+fprintf('1-3 are MISA problems\n');
+fprintf('1------DEB\n');
+fprintf('2------SCH\n');
+fprintf('3------KUR\n');
+fprintf('4-18 are Veldhuizen probems\n');
+fprintf('4------Binh (1\n');
+fprintf('5------Binh (3\n');
+fprintf('6------Fonseca\n');
+fprintf('7------Fonseca (2\n');
+fprintf('8------Laumanns\n');
+fprintf('9------Lis\n');
+fprintf('10------Murata\n');       
+fprintf('11------Poloni\n');
+fprintf('12------Quagliarella\n');
+fprintf('13------Rendon\n');
+fprintf('14------Rendon (2\n');
+fprintf('15------Schaffer\n');
+fprintf('16------Viennet\n');
+fprintf('17------Viennet (2\n');
+fprintf('18------Viennet (3\n');
+fprintf('19-24 are DTLZ Problems\n');
+fprintf('19------DTLZ1\n');
+fprintf('20------DTLZ2\n');
+fprintf('21------DTLZ3\n');
+fprintf('22------DTLZ4\n');
+% fprintf('23------DTLZ5\n');
+fprintf('24------DTLZ6\n');
+fprintf('25-30 are ZDT Problems\n');
+fprintf('25------ZDT1\n');
+fprintf('26------ZDT2\n');
+fprintf('27------ZDT3\n');
+fprintf('28------ZDT4\n');
+% fprintf('29------ZDT5\n');
+fprintf('30------ZDT6\n');
